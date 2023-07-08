e479573adf21c65ead189153e792f306a924c6b7
yıldız <mesel4461@gmail.com>
2023-07-08 21:27:23 +0300

    end

--- main/info.py
+++ main/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'main.demo.django.login'
-EMAIL_HOST_PASSWORD = 'maindemo123'
+EMAIL_HOST_USER = 'gfg.demo.django.login'
+EMAIL_HOST_PASSWORD = 'gfgdemo123'
 EMAIL_PORT = 587
