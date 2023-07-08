2a0712dbb9482a1ebc29c56d62247d55f55d7cc9
yıldız <mesel4461@gmail.com>
2023-07-08 17:41:57 +0300

    core prepared

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse("hello ı am working")
+def signup(request):
+    return render(request,"authentication/signup.html")
+def signin(request):
+    return render(request,"authentication/signin.html")
+def signout(request):
+    pass
